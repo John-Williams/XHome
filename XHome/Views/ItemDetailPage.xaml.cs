3fd835b91e268e7aea603f2017b95e0df172ec25
John Williams <jcwilliams@gmail.com>
2021-02-05 11:16:15 +0000

    initial commit

---
+++ XHome/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XHome.ViewModels;
+
+namespace XHome.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
